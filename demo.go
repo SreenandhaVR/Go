f098dd9cbd4e13fb3a1328d14cac844604717840
Sreenandha V R <sreenandhavr161@gmail.com>
2026-06-19 16:03:28 +0530

    First Program

--- demo.go
+++ demo.go
@@ -1,6 +1,13 @@
-package main
+// package main
 
-import "fmt"
-func main() {
-	fmt.Println("Hello, World!")
+// import "fmt"
+// func main() {
+// 	fmt.Println("Hello, World!")
+// }
+
+package main
+ import "fmt"
+  func main() {
+	var x string = "hello"
+	fmt.Println(x)
 }
