0d0584df6b7692944595a38ae5f05ac40d17dca6
Sreenandha V R <sreenandhavr161@gmail.com>
2026-06-20 12:46:22 +0530

    Variables in g

---
+++ variables/demo.go
@@ -0,0 +1,19 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	x := false
+// 	fmt.Printf("%T %T", x, x)
+// }
+
+package main
+
+import "fmt"
+
+func main() {
+	x := 7
+	//fmt.Printf("%v", x)
+	//fmt.Printf("the value of x is %v", x)
+	fmt.Printf("%b", x)
+}
